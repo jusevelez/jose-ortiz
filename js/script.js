29bad27df78f2cc518cae7b050cfad07cdbd5d80
Juan Velez <hola@jsvelez.com>
2020-09-11 16:57:20 -0500

    Ajuste form y redes sociales- 6

--- js/script.js
+++ js/script.js
@@ -61,3 +61,66 @@ function validateForm() {
   }
 }
 
+// Form mobile
+
+// function validateNombre() {
+//   var name = document.getElementById('nombre').value;
+//   if(name.length == 0) {
+//     alert("Por favor ingresa tu nombre") ;
+//     return false;
+
+//   }
+//   if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+//     alert("Por favor ingresa un nombre correcto") ;//Validation Message
+//     return false;
+//   }
+//   return true;
+// }
+
+// function validateTelefono() {
+//   var phone = document.getElementById('telefono').value;
+//   if(phone.length == 0) {
+//     alert("Por favor ingresa un numero de teléfono") ;//Validation Message
+//     return false;
+//   }
+
+// //   if(!phone.match(/^[0]?[789]\d{9}$/)) {
+// //    alert("Por favor ingresa un numero de teléfono") ;//Validation Message
+// //    return false;
+// //  }
+
+//  return true;
+
+// }
+
+// function validateCorreo () {
+
+// var email = document.getElementById('correo').value;
+// if(email.length == 0) {
+//   alert("Por favor ingresa su correo electrónico") ;//Validation Message
+//   return false;
+
+// }
+
+// // if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+// //   alert("Por favor ingresa una cuenta de correo valida") ;//Validation Message
+// //   return false;
+
+// // }
+
+// return true;
+
+// }
+
+
+// function validateFormulario() {
+// if (!validateName() || !validatePhone() || !validateEmail()) {
+
+//   alert("Por favor verifica nuevamente la información ingresada");//Validation Message
+//   return false;
+// }
+// else {
+//   submitted=true;
+//   return true;
+// }
+// }
